6ef5d7d3c5008bdc0bb7f1e3d95a369769e6b637
NguyenSiVanHao <HaoNSVSE172181@fpt.edu.vn>
2026-02-06 21:57:59 +0700

    [SMA-170] Design HomePage (fix fail deploy github 2)

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-import Hero from './components/HeroHome';
+import Hero from './components/Hero';
 import Features from './components/FeaturesHome';
 import RecruiterSection from './components/RecruiterSectionHome';
 import Footer from './components/FooterHome';
